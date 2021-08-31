acd7167b6757d0e9e5720bdef7d0e342289fe11a
Mohammed1994Mosleh <mohammed1994mosleh@gmail.com>
2021-09-01 00:35:15 +0300

    css for card

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -8,24 +8,26 @@ class Header2 extends React.Component {
   render() {
     return (
       <>
-        <header id='header'>
+       <header id='header'>
           <div className='intro'>
             <div className='overlay'>
               <div className='container'>
                 <div className='row'>
                   <div className='col-md-8 col-md-offset-2 intro-text'>
-
-                    <Form onSubmit={this.props.getUniversity}>
+                     <div style={{position:"relative",right:"185px" }}>
+                    <Form style={{}} onSubmit={this.props.getUniversity}>
 
                       <Form.Group className="mb-3" >
-                        <Form.Label >Country Name</Form.Label>
-                        <Form.Control type="text" placeholder="Write Country Name" name="city" />
+                        <Form.Label style={{fontSize: "30px" , color:"#FFFFFF" , padding:"2%",position: "relative", right: "15px"}} >Country Name</Form.Label>
+                      
+                        <Form.Control style={{width:"550px", padding:"2%"}} type="text" placeholder="Put your steps here" name="city" />
                       </Form.Group>
-                      <Button variant="primary" type="submit">
+                      <Button style={{borderRadius: "35px",margin:"10px" , padding: "12px 30px", fontSize:"15px",position: "relative", right: "15px", color:"White" }} variant="primary" type="submit">
                         Search
                       </Button>
                     </Form>
-                    
+                    </div>
+
                   </div>
                 </div>
               </div>
